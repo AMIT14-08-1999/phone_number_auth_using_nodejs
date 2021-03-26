6c1975f18b89ac9954a0128ce6fdbb41d3430668
Amit <amit1999ghosh99@gmail.com>
2021-03-26 20:37:58 +0530

    phone number authentication using nodejs ans twilio

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    serviceID: "VAd2af255d4126e7ebbcd3895a7829e7e1",
+    accountSID: "ACc1a1d6f01e90ecda0730650b6fd42ed1",
+    authToken: "38da6393b3438368ce35f1132905c60a",
+}
